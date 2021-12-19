d054e9fea72fd310247263b3fde5891b8fcdb41a
a.werle <anatolij.werle@vitasystems.de>
2021-12-19 17:03:52 +0100

    (red) add plug to assign the current user's email to the conn, use it in connection to user_socket

--- chatter/assets/js/user_socket.js
+++ chatter/assets/js/user_socket.js
@@ -1,6 +1,6 @@
 import { Socket } from "phoenix"
 
-let socket = new Socket("/socket", { params: {} });
+let socket = new Socket("/socket", { params: { email: window.email } });
 socket.connect();
 
 export default socket
