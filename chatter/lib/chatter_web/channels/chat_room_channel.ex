a17ed448750af59e4ba2e7d4eb9760a0df016fb6
a.werle <anatolij.werle@vitasystems.de>
2021-12-19 17:13:37 +0100

    (red) save new messages, also assume room information to exist on the socket assigns

--- chatter/lib/chatter_web/channels/chat_room_channel.ex
+++ chatter/lib/chatter_web/channels/chat_room_channel.ex
@@ -1,14 +1,20 @@
 defmodule ChatterWeb.ChatRoomChannel do
   use ChatterWeb, :channel
 
+  alias Chatter.Chat
+
   def join("chat_room:" <> _room_name, _msg, socket) do
     {:ok, socket}
   end
 
   def handle_in("new_message", payload, socket) do
-    author = socket.assigns.email
+    %{room: room, email: author} = socket.assigns
+
     outgoing_payload = Map.put(payload, "author", author)
+
+    Chat.new_message(room, outgoing_payload)
     broadcast(socket, "new_message", outgoing_payload)
+
     {:noreply, socket}
   end
 end
